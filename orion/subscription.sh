a2c460761987a960217088cb58d9226b94003119
frbattid <francisco.romerobueno@telefonica.com>
2017-03-22 10:47:42 +0100

    Add Orion subscription

---
+++ orion/subscription.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -v -X POST "http://localhost:1026/v2/subscriptions" -H "Fiware-Service: icai" -H "Fiware-ServicePath: /urbanization" -H "Content-Type: application/json" -d @- <<EOF
+{
+  "subject": {
+    "entities": [
+      {
+        "idPattern": "swm.*",
+        "type": "smartWaterMeter"
+      }
+    ]
+  },
+  "notification": {
+    "http": {
+      "url": "http://localhost:5050/notify"
+    },
+    "attrsFormat": "legacy"
+  }
+}
+EOF
